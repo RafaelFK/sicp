29d51cffcd657b13103d386129b578e99648c66f
Rafael F. Katopodis <rafaelkatopodis@gmail.com>
2017-12-19 15:06:31 -0200

    Start section 1.2

--- chp1.rkt
+++ chp1.rkt
@@ -12,8 +12,6 @@
       x
       (- x)))
 
-
-
 (define (sqrt x)
   (define (sqrt-iter guess)
     (if (good-enough? guess)
@@ -30,6 +28,29 @@
   (< (abs (- (square guess) x)) 0.001))
   
   (sqrt-iter 1.0))
+
+#| Linear recursive implementation |#
+#|
+(define (factorial n)
+  (if (= n 1)
+      n
+      (* n (factorial (- n 1)))))
+|#
+
+#| Linear iterative implementation |#
+(define (factorial n)
+  (define (fact-iter product counter)
+    (if (> counter n)
+        product
+        (fact-iter (* product counter) (+ counter 1))))
+
+  (fact-iter 1 1))
+
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
 
 #| Exercise 1.3 |#
 
@@ -101,7 +122,70 @@
   (define (new-good-enough? guess previous-guess)
   (< (abs (- guess previous-guess)) .0000000001))
   
-  (define (improve-cube-root guess x)
+  (define (improve-cube-root guess)
     (/ (+ (/ x (square guess)) (* 2.0 guess)) 3.0))
 
-  (cube-root-iter 1.0 0.0 x))
+  (cube-root-iter 1.0 0.0))
+
+#| Exercise 1.9 |#
+
+#|
+
+(define (+ a b)
+  (if (= a 0)
+       b
+       (inc (+ (dec a) b))))
+
+(+ 4 5)
+(inc (+ 3 5))
+(inc (inc (+ 2 5)))
+(inc (inc (inc (+ 1 5))))
+(inc (inc (inc (inc (+ 0 5)))))
+(inc (inc (inc (inc 5))))
+(inc (inc (inc 6)))
+(inc (inc 7))
+(inc 8)
+9
+
+Linear recursive process
+|#
+
+#|
+
+(define (+ a b)
+  (if (= a 0)
+      b
+      (+ (dec a) (inc b))))
+
+(+ 4 5)
+(+ 3 6)
+(+ 2 7)
+(+ 1 8)
+(+ 0 9)
+9
+
+Linear iterative process
+|#
+
+#| Exercise 1.10 |#
+
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
+
+(define (f n) (A 0 n))
+(define (g n) (A 1 n))
+(define (h n) (A 2 n))
+(define (k n) (* 5 n n))
+
+#|
+
+f(n) = 2n
+g(n) = 2^n
+h(n) = 2^...^2 (n times)
+k(n) = 5n^2
+
+|#
